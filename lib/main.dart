6ada09e6076645e9e451d5613209767205b8b2a5
dannythompson901 <dannythompson901@gmail.com>
2019-10-29 18:11:17 -0500

    dice changing app to settle bets!

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.yellow.shade900,
+        appBar: AppBar(
+          backgroundColor: Colors.yellow.shade900,
+          title: Text('Dicee'),
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceImageChanger = 1;
+  var rightDiceImageChanger = 1;
+
+  void diceChangeMethod() {
+    setState(() {
+      leftDiceImageChanger = Random().nextInt(6) + 1;
+      rightDiceImageChanger = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext buildContext) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FlatButton(
+                onPressed: () {
+                  diceChangeMethod();
+                },
+                child: Image.asset('images/dice$leftDiceImageChanger.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FlatButton(
+                onPressed: () {
+                  diceChangeMethod();
+                },
+                child: Image.asset('images/dice$rightDiceImageChanger.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
